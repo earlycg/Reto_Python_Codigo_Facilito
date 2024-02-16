03b70ecbcb1a1d5d924adcc1519dd568840ce1a5
Early Start <125206561+earlycg@users.noreply.github.com>
2024-02-16 00:25:04 -0500

    Create Reto_1er_día.py
    Start the first day of the Python challenge with a basic exercise.

---
+++ Reto_1er_día.py
@@ -0,0 +1,7 @@
+#Challenge 1 Day Monday
+names = input('Enter your name(s)\n>')
+last_names = input('Enter your last name(s)\n>')
+phone_number = int(input('Enter your phone number\n>'))
+email = input('Enter your email address\n>')
+
+print(f'Hello {names} {last_names}, you will soon receive an email at {email}.\n> I love Code Facilito')
